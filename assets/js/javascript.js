dc4f126ccf9aa72400beab2e3a105c98580de115
Claire Cashmore <clairecashmore2017@gmail.com>
2022-06-06 13:20:18 -0400

    added querys to retrieve form values

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,13 +1,35 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
 
+var formEl = document.querySelector("#task-form"); 
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-console.log(tasksToDoEl);
 
-function createTaskHandler() {
-    var taskItemEl = document.createElement("li");
-    taskItemEl.textContent = "hello";
-    tasksToDoEl.appendChild(taskItemEl);
-    taskItemEl.className = "task-item";
-}
-buttonEl.addEventListener("click",  createTaskHandler);
+function createTaskHandler(event) {
+    event.preventDefault(); 
+
+    // Retrieving the text value created by the user
+    var taskNameInput = document.querySelector("input[name ='task-name']").value;
+    //Retrieving the type of task the user chose
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    //Creating the list item and fiving class name
+    var listItemEl = document.createElement("li"); 
+    listItemEl.className = "task-item"; 
+
+    //create a div to holde task info and add it to list item
+    var taskInfoEl = document.createElement("div");
+    //give it a class name
+    taskInfoEl.className = "task-info";
+
+    //add html content to the div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    // listItemEl.textContent = taskNameInput; 
+    listItemEl.appendChild(taskInfoEl);
+
+    //add to the parent
+    tasksToDoEl.appendChild(listItemEl); 
+    }; 
+
+    formEl.addEventListener("submit", createTaskHandler);
+ 
+    
+ 
+      
